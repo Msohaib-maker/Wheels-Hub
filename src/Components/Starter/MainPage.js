bb99e6d8012cde352306a8809f4d5388b9b2a418
Msohaib-maker <afzalsohaib59@gmail.com>
2024-10-17 01:16:00 +0500

    Main Page ReDesign 1.0

--- src/Components/Starter/MainPage.js
+++ src/Components/Starter/MainPage.js
@@ -7,10 +7,6 @@ const images = [
     './car1.jpeg',
     './car2.jpeg',
     './mercedes-benz-car.jpg',
-    './car1.jpeg',
-    './car2.jpeg',
-    './mercedes-benz-car.jpg',
-    './car1.jpeg',
     // Add more image paths here if needed
 ];
 
@@ -21,24 +17,30 @@ function MainPage(props) {
         <div>
             
             <div className='container_MP'>
-                <img src="./car-wheel.png" class="backgroundimage_mp " alt="Background Image"></img>
+                <img src="./car-wheel.png" class="lead_backgroundimage " alt="Background Image"></img>
                 <div class="overlay-text">
                     <h2>Welcome to ApnaWheels</h2>
                     <h3 className='text-color'>Sponsered by CarTech</h3>
                 </div>
+                <div className='overlay-button'>
+                    <button className='button-style'>Shop by Brands</button>
+                </div>
+                
             </div>
             <br/><br/>
-            <center><h3><i>Buy and Sell New and Used Car</i></h3></center>
-            <br/><br/>
-            <div className="image-slider">
-                {images.map((image, index) => (
-                    <img
-                    key={index}
-                    src={image}
-                    alt={`Car ${index + 1}`}
-                    class="backgroundimage_mp" 
-                    />
-                ))}
+            <h3 className='main-text'>Auto Part and Accesories</h3>
+            
+            
+
+            <div class="container12">
+                {images.map((src, index) => {
+                    return (
+                        <div className="child" key={index}>
+                            <img src={src} className='img-talk' alt={`Image ${index + 1}`} />
+                            <p>Car motor</p>
+                        </div>
+                    );
+                })}
             </div>
 
             <br/><br/>
